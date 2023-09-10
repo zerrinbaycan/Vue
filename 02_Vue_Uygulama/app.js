0c47b343d9b05ccb51a349fb010b697e6271f3d3
zerrinbaycan <109226843+zerrinbaycan@users.noreply.github.com>
2023-09-10 17:36:14 +0300

    Add files via upload
    .

---
+++ 02_Vue_Uygulama/app.js
@@ -0,0 +1,7 @@
+
+const app = Vue.createApp({
+    template:'<h2>Div Id üzerinden görüntüleme</h2>'
+});
+
+
+app.mount('#app')
---
+++ 02_Vue_Uygulama/app2.js
@@ -0,0 +1,7 @@
+
+const app2 = Vue.createApp({
+    template:'<h2>Div Class üzerinden görüntüleme</h2>'
+});
+
+
+app2.mount('.app2')
