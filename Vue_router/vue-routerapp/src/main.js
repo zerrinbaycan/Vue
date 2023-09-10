a9af265109bbb5d6d912c954b8468f26f44c2070
zerrinbaycan <zerrinbaycan@gmail.com>
2023-09-11 01:33:49 +0300

    .

--- Vue_router/vue-routerapp/src/main.js
+++ Vue_router/vue-routerapp/src/main.js
@@ -2,10 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount('#app');
 
-//createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
