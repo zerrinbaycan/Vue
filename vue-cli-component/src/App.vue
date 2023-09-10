71b428ba438b5737784c377f691eee10f2b928d9
zerrinbaycan <109226843+zerrinbaycan@users.noreply.github.com>
2023-09-10 22:33:59 +0300

    Add files via upload

---
+++ vue-cli-component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Title/>
+    <h3>App.vue Başlık</h3>
+  </div>  
+</template>
+
+<script>
+import Title from './components/Title.vue'
+export default {
+  name: 'App',
+  components:{
+    Title
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-component/src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            title:"Vue 3'e Giriş"
+        }
+        
+    }
+}
+</script>
+/* scoped: sadece bu sayfada geçerli olsun demek!*/
+<style scoped>
+h3{
+  color: orangered;
+  font-size: 30px;
+  font-weight: 700;
+}
+</style>
