6d57851f65c13d118512d589603114cfc0a9cfd6
zerrinbaycan <109226843+zerrinbaycan@users.noreply.github.com>
2023-09-10 22:35:04 +0300

    Add files via upload

---
+++ vue-cli-comp-veriaktarimi/src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>  
+  <div @click.alt="clean">
+    <h3>Temizle</h3>
+  </div>
+</template>
+// click.alt,click.shift  -> shifte,alta basılı iken çalışsın 
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        clean(){
+            this.$emit('clean')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli-comp-veriaktarimi/src/components/Title.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h3>{{ titleText }}</h3>
+      <h4>{{ text }}</h4>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props:['titleText','text'],
+      data() {
+          return{
+          }
+          
+      }
+  }
+  </script>
+  /* scoped: sadece bu sayfada geçerli olsun demek!*/
+  <style scoped>
+  h3{
+    color: orangered;
+    font-size: 30px;
+    font-weight: 700;
+  }
+  </style>
