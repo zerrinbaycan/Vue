65768baff7ef127d289f2f651d4ac6e488262c19
zerrinbaycan <zerrinbaycan@gmail.com>
2023-09-12 08:15:35 +0300

    .

---
+++ Vue_router/vue-routerapp/src/views/StudentInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>Öğrenci Bilgi Sayfası</h3>
+    <p>Öğrenci Numarası:{{id}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    /*router içindeki index dosyasında props:true ise buraya ihtiyacımız olmuyor.props yazıyruz data yerine
+    data(){
+        return{
+            studentId:this.$route.params.id
+        }
+    }*/
+    props:["id"]
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue_router/vue-routerapp/src/views/Students.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Öğrenciler routhe path üzerinden</h1>
+        <div v-for="student in students" :key="student.id">
+            <router-link :to="{path:`/students/${student.id}`}">{{student.name}}</router-link>             
+        </div>
+
+        <h1>Öğrenciler - route name üzerinden</h1>
+        <div v-for="student in students" :key="student.id">
+            <router-link :to="{name:'studentInfo',params:{id:student.id}}">{{student.name}}</router-link>             
+        </div>
+    </div>  
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            students:[
+                {id:1,name:"Zerrin",not:60},
+                {id:2,name:"Berna",not:75},
+                {id:3,name:"Ayşe",not:80},
+                {id:4,name:"Anıl",not:100}
+            ]
+        }        
+    },
+}
+</script>
+
+<style>
+
+</style>
